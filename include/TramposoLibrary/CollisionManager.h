ff39892716f02024ee20b509f89751aa88d65f92
Tramposo <tramposo1312@users.noreply.github.com>
2024-09-08 00:18:34 +0100

    feat: Implement CollisionManager
    - Add basic rectangle collision detection
    - Implement collider management (add, remove, update)
    - Add method to check all collisions in the scene

--- include/TramposoLibrary/CollisionManager.h
+++ include/TramposoLibrary/CollisionManager.h
@@ -2,14 +2,23 @@
 #define TRAMPOSO_COLLISION_MANAGER_H
 
 #include <SDL.h>
+#include <string>
+#include <unordered_map>
 
 namespace TramposoLibrary {
 
     class CollisionManager {
     public:
-        bool checkCollision(const SDL_Rect& rectA, const SDL_Rect& rectB);
+        void addCollider(const std::string& name, const SDL_Rect& rect);
+        void removeCollider(const std::string& name);
+        void updateCollider(const std::string& name, const SDL_Rect& rect);
+        bool checkCollision(const std::string& name1, const std::string& name2);
+        void checkCollisions();
+
+    private:
+        std::unordered_map<std::string, SDL_Rect> m_colliders;
     };
 
-} // namespace TramposoLibrary
+} // TramposoLibrary
 
 #endif // TRAMPOSO_COLLISION_MANAGER_H
--- src/CollisionManager.cpp
+++ src/CollisionManager.cpp
@@ -2,11 +2,45 @@
 
 namespace TramposoLibrary {
 
-    bool CollisionManager::checkCollision(const SDL_Rect& rectA, const SDL_Rect& rectB) {
-        return (rectA.x < rectB.x + rectB.w &&
-            rectA.x + rectA.w > rectB.x &&
-            rectA.y < rectB.y + rectB.h &&
-            rectA.y + rectA.h > rectB.y);
+    void CollisionManager::addCollider(const std::string& name, const SDL_Rect& rect) {
+        m_colliders[name] = rect;
+    }
+
+    void CollisionManager::removeCollider(const std::string& name) {
+        m_colliders.erase(name);
+    }
+
+    void CollisionManager::updateCollider(const std::string& name, const SDL_Rect& rect) {
+        m_colliders[name] = rect;
+    }
+
+    bool CollisionManager::checkCollision(const std::string& name1, const std::string& name2) {
+        auto it1 = m_colliders.find(name1);
+        auto it2 = m_colliders.find(name2);
+
+        if (it1 == m_colliders.end() || it2 == m_colliders.end()) {
+            return false;
+        }
+
+        const SDL_Rect& rect1 = it1->second;
+        const SDL_Rect& rect2 = it2->second;
+
+        return (rect1.x < rect2.x + rect2.w &&
+            rect1.x + rect1.w > rect2.x &&
+            rect1.y < rect2.y + rect2.h &&
+            rect1.y + rect1.h > rect2.y);
+    }
+
+    void CollisionManager::checkCollisions() {
+        for (auto it1 = m_colliders.begin(); it1 != m_colliders.end(); ++it1) {
+            auto it2 = it1;
+            ++it2;
+            for (; it2 != m_colliders.end(); ++it2) {
+                if (checkCollision(it1->first, it2->first)) {
+                    // Collision detected so Trigger an event or callback here
+                }
+            }
+        }
     }
 
 } // TramposoLibrary
